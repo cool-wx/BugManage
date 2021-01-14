ca95f0c7032d31bb5ee3864bf0e950d644d919d6
coolboy-wx <923843243@qq.com>
2021-01-14 17:25:47 +0800

    下班

--- models/bug_bug.py
+++ models/bug_bug.py
@@ -13,6 +13,13 @@ class Bug(models.Model):
                                     string='关闭原因')
     user_id = fields.Many2one('res.users', string='负责人', default=lambda self: self.env.user)
     follower_id = fields.Many2many('res.partner', string='关注者')
+    display_detail = fields.Char(string='detail', compute='_compute_display_detail')
+
+    @api.depends('detail')
+    @api.onchange('detail')
+    def _compute_display_detail(self):
+        for rec in self:
+            rec.display_detail = (rec.detail[:20] + '……') if len(rec.detail) > 20 else rec.detail
 
     def do_close(self):
         for bug in self:
